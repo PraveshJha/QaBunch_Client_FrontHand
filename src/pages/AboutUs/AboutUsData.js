5e4bf2ac7b781b6f9a12ea7ceadb3f5607013090
Pravesh Jha <pravesh28@gmail.com>
2023-06-12 16:03:58 +0530

    added
    added

---
+++ src/pages/AboutUs/AboutUsData.js
@@ -0,0 +1,6 @@
+export var AboutUsData = {
+	"paragraph": "UI driven AI-Powered Codeless Test Automation solution on the Cloud",
+	"oneStopSolutionText":"QAautoMATER is end to end one stop solution. It is UI driven codeless QA solution on cloud which supports ML algorithm. ",
+	"toolCostText":"Best Result at the most affordable price",
+	"latestupdatetext":"Native Mobile App testing support",
+};
